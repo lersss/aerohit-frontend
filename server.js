a1b21366c1b4cdbee75ad146897a56c3d62ef6a2
lersss <skycomposer@gmail.com>
2026-09-07 20:52:19 +0700

    fix: правильный amvera.yaml и Express-сервер для SPA

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Раздаём статику из папки build
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Все запросы, кроме статики, отдаём index.html (SPA)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
